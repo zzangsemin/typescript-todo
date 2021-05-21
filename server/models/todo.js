917258e902edafe025043d9ab872c45b219bb33c
zzangsemin <1015tpals@gmail.com>
2021-05-21 16:12:52 +0900

    [SERVER]Feat: 모델 생성

---
+++ server/models/todo.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const todoSchema = new Schema({
+  content: String,
+  flag: Boolean,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+export default Todo;
